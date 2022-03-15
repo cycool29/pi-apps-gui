9ae0792f5e5785b6a20f3fc7e64ab1b8f7dec6de
cycool29 <cycool29@gmail.com>
2022-03-15 18:36:02 +0800

    Update pi-apps.py

--- pi-apps.py
+++ pi-apps.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 # Import modules
+import signal
+import sys
 import random
 import textwrap
 import webbrowser
@@ -81,6 +83,7 @@ def show_category(category):
     global app_list_data
     global app_list
     global current_category
+    global current_app
     if category == 'All Apps':
         category_apps = os.popen(
             f'{DIRECTORY}/api list_apps cpu_installable | {DIRECTORY}/api list_intersect "$({DIRECTORY}/api list_apps visible)" | {DIRECTORY}/api list_intersect "$({DIRECTORY}/api list_apps cpu_installable)"').read().split('\n')
@@ -108,6 +111,8 @@ def show_category(category):
             i += 1
 
     window['-APP LIST-'].update(app_list_data)
+    print(current_app)
+    current_app = ''
     window['-MENU BACK-'].update(visible=True)
     window['-SEARCH BAR-'].update('')
     window['-TOOLTIP-'].update(first_line)
@@ -119,8 +124,9 @@ def load_app_info(app):
     global website_text
     global current_app
     global description_text
+    global button_list
     status_text = os.popen(
-        f'{DIRECTORY}/api app_status "' + app + '"').read().rstrip('\n')
+        f'{DIRECTORY}/api app_status "' + app + '" 2>/dev/null').read().rstrip('\n')
     if status_text == '':
         status_text = 'uninstalled'
     website_text_original = textwrap.wrap(os.popen(f'cat "{DIRECTORY}/apps/' +
@@ -136,8 +142,7 @@ def load_app_info(app):
     description_text = os.popen(f'cat "{DIRECTORY}/apps/' +
                                 app + '/description"').read()
 
-    users_count = int(
-        os.popen(f"{DIRECTORY}/api usercount '{app}' 2>/dev/null").read().rstrip('\n'))
+    users_count = int(os.popen(f"{DIRECTORY}/api usercount '{app}' 2>/dev/null").read().rstrip('\n'))
 
     window["-APP NAME-"].update(app,
                                 font=default_font_name + ' ' + str(20) + ' bold')
@@ -165,27 +170,96 @@ def load_app_info(app):
     window["-GITHUB BUTTON-"].update(visible=False)
     window["-WEBSITE BUTTON-"].update(visible=False)
     window["-DESCRIPTION-"].update(description_text)
-    if os.popen(f'{DIRECTORY}/api app_type ' + app).read().strip('\n') != 'package':
-        window["-SCRIPTS-"].update(visible=True)
-    else:
-        window["-SCRIPTS-"].update(visible=False)
-    window["-CREDITS-"].update(visible=True)
-    window["-UNINSTALL-"].update(visible=False)
-    window["-ERRORS-"].update(visible=True)
-    window["-ERRORS-"].update(visible=False)
 
-    if status_text == 'corrupted':
-        window["-ERRORS-"].update(visible=True)
-        window["-INSTALL OR UNINSTALL-"].update(
-            visible=True, image_filename=f'{DIRECTORY}/icons/install.png')
-        window["-UNINSTALL-"].update(visible=True,
-                                     image_filename=f'{DIRECTORY}/icons/uninstall.png')
+    buttons = ['-BUTTON 1-', '-BUTTON 2-', '-BUTTON 3-',
+               '-BUTTON 4-', '-BUTTON 5-', '-BUTTON 6-']
+
+    for button in buttons:
+        window[button].update(' ', visible=False, image_filename='')
+
+    button_list = []
+    i = []
+
+    if status_text == 'installed':
+        # uninstall button
+        i.append('uninstall')
+        # credits
+        if os.path.exists(f'{DIRECTORY}/apps/{app}/credits'):
+            i.append('credits')
+        # scripts
+        if os.popen(f'{DIRECTORY}/api app_type ' + app).read().strip('\n') != 'package':
+            i.append('scripts')
+        # edit
+        if show_edit_button is True:
+            i.append('edit')
+
     elif status_text == 'uninstalled':
-        window["-INSTALL OR UNINSTALL-"].update(
-            visible=True, image_filename=f'{DIRECTORY}/icons/install.png')
-    elif status_text == 'installed':
-        window["-INSTALL OR UNINSTALL-"].update(
-            visible=True, image_filename=f'{DIRECTORY}/icons/uninstall.png')
+        # install button
+        i.append('install')
+        # credits
+        if os.path.exists(f'{DIRECTORY}/apps/{app}/credits'):
+            i.append('credits')
+        # scripts
+        if os.popen(f'{DIRECTORY}/api app_type ' + app).read().strip('\n') != 'package':
+            i.append('scripts')
+        # edit
+        if show_edit_button is True:
+            i.append('edit')
+
+    elif status_text == 'corrupted':
+        i.append('install')
+        i.append('uninstall')
+        # credits
+        if os.path.exists(f'{DIRECTORY}/apps/{app}/credits'):
+            i.append('credits')
+        # scripts
+        if os.popen(f'{DIRECTORY}/api app_type ' + app).read().strip('\n') != 'package':
+            i.append('scripts')
+        # edit
+        if show_edit_button is True:
+            i.append('edit')
+        i.append('errors')
+
+    n = 0
+
+    for button in i:
+        if button == 'install':
+            window[buttons[n]].update(
+                '      ', visible=True, image_filename=f'{DIRECTORY}/icons/install.png', )
+            window[buttons[n]].set_tooltip('Install ' + app)
+
+            button_list.append('install')
+        elif button == 'uninstall':
+            window[buttons[n]].update(
+                '  ', visible=True, image_filename=f'{DIRECTORY}/icons/uninstall.png', )
+            button_list.append('uninstall')
+            window[buttons[n]].set_tooltip('Uninstall' + app)
+        elif button == 'credits':
+            window[buttons[n]].update("Credits", visible=True)
+            window[buttons[n]].set_tooltip(
+                'See who made the app and who put it on Pi-Apps')
+            button_list.append('credits')
+        elif button == 'scripts':
+            window[buttons[n]].update(
+                '  ', visible=True, image_filename=f'{DIRECTORY}/icons/shellscript.png',)
+            window[buttons[n]].set_tooltip(
+                "Feel free to see how an app is installed or uninstalled!\nPerfect for learning or troubleshooting.")
+            button_list.append('scripts')
+        elif button == 'edit':
+            window[buttons[n]].update(
+                visible=True, image_filename=f'{DIRECTORY}/icons/edit.png', )
+            window[buttons[n]].set_size((0, 24))
+            print(type(window))
+            button_list.append('edit')
+            window[buttons[n]].set_tooltip('Make changes to the app')
+        elif button == 'errors':
+            window[buttons[n]].update(
+                visible=True, image_filename=f'{DIRECTORY}/icons/log-file.png')
+            window[buttons[n]].set_tooltip('')
+            button_list.append('errors')
+
+        n += 1
+
     current_app = app
 
 
@@ -256,7 +330,15 @@ for category in sorted(app_categories):
     i += 1
 current_category = 'Category List'
 
-github_image = 'iVBORw0KGgoAAAANSUhEUgAAAHcAAAB3CAMAAAAO5y+4AAAAb1BMVEX///8bHyMAAAARFhsYHCAAAAoUGR4ABg4LERf09PQAAAefn58AAAPe3t7x8fEOFBnr6+uBgYLMzc2VlpcpLC6vr7DW1tfl5eVnaWrCw8NVVlenp6g6Ozx1dnhQUVKMjI1gYWJERki3uLgiJik0NjqNkMtOAAAGlklEQVRogd1b24KqMAyUUKFclKsKCKyo//+NB0SxtClQqC9n3hbcjm3aZJLU3U4VYXz3oqwuH4ZhpGWdRd49viiPooRjnjQ2gG/TwCJGB2IFtH0Cdp3k4W9Iz15DYG/2fDyIuQfSeGfdpIduohLOLzfQxjvoI3XywvatSc4PLKBF7mhhPV5LCBaR9giguR43szreA5ZNlZ106m2c89WAaaPiIJCeNrCey1WsPXO5enNXyivMwoJs1WLHgb2BtYNNY3XaaNNkexD4U2S91LCZtQPUSq47TqkWWsOgqcJanxZ6pyUw3cUnKll9ejAQ8JbR/ukx7RfLdlekm3YZsfbZLiP2fkHbEs/Y+P4b2pb4PkUbuzp3MgviTpzjS6rv3PIwU7nnqjkvRVuhaK2Kv1YnO7nBahktf4Ls6uRFjQGqPtMEUlbXE79FIZIYl99T8Hoc3gt3r8BK3ebUL2lhcuOhJnYoZ9wgG+yewFJmCtWgZE/cRCyC8VZ8mAfWoScwek0sM6CUBoE5+rIBVEzWcOQX0K5E2rNwcmG0AZ0IumUjZpeWwKO5ZVVUZVnxdNs/90FHT6AYq3ZB6IOouUrhCAH/zWrwwbj9nc5jeewc7kn2BHAJ7xsqfkdaJU/L2wL7zM5LDjKp5oSnSFDrnrArgAvGzkM4pjTjh1GG6HWJMf7iSDiwJcdNAbkvjDoOEEdxuoatqgURXlcYlaSsNa5IGPrNfA24ft874mb+kX277fq1cI5FXavZzCvu527C+fC+QPNb/vyqI8MiSlB8Xh/wRAg2lwxsLIYS+hk3wcXNfqHulUJ0vf18kvf7BlcZykkVD3TbfDfOGV9mun1fSRaS9tEBl66EaKiDFahUefusxkRfbilQfBASbGeZjfwdLebGXAR0LYnR+Urc+q6mkt8T27N+5zoSzKnQmx5aJK632HcnCTUvLv1W4JgiVny5LMypsM57IwSt04LYrUbFFgITfiuBbh8IRbX+ep7PD7gQIUXWszXjHYnNnITdBKdGdnSblXqIlyTP7YXcAZiBbW8XIY9NPU6jxx8yMRrtMiTmB9slzheYy2oJsOWn+rYz7jmsYlci200rLybuSLN7iE9/P18j3SEP9doX5X3gvDeNvLisQNfZlJZAVgA7Ry1vijzUGBZwv9HaF9vPxNTYY7whDoKUuwITBNsl+wAHU8nt+cX81Uw1UQkhJmfaA4P5Zx056Ado0tD6Zywe6QwMiZh7v+IRWvklRFsA5ot2HzuiekOfgS+oOm/1BqqvfqxjO30lSVJ1LXSNLXOnJ2XpUTI75BLgVnz5YTRfaCesRWLhCeErX5Bkx3sdR1gy9ksm4/kgWjtVhoFWEvp8UGJgw3psphVK2j36/FfaqrK3qg6sCvhayT7fl9Q3NhdWpD2wd31DVs/ZSHyXdXOHQqCk7NIRZ6tPkydtIg+lYEm9roOt0iZnEN6kc/nW68YxI6AB802tUYdkKZKJXhcjVpnjbfq36may/0YhUlQ9njvVVWVy62/9+d3MOGesdSjUp8WTzivYT7UUrYbRqsMRJun7aU7YtTd9Wnvx3B5zzqdb+8kJUmNcb2f6C7R5j35Jx4LP9N1HmXl3fM3D/Fo1qQ1YUWGEcX+BOUr08ZYaodDtIhaFAI/LTrGwY8s13B1j+Cfy6ZdiSlAaLBxJdBGmy2UirC/9uCnRn0zo22U3A8RqK9tS+bx98pEKJvImY8GEkd4fu6rE7q144XzdZH6KNk/4742YiQ2WnypwPib2p2rSh/mFxsuAAbvS700djxzedDlt9g6GFaD/x2q/wRBh1h6Q7kl3ReE5RYtLVhayIi97n+GbMJyjsutw0zqaCU1ogs3SSqM5c3/DZLopTng4XMLZEgBeUBggv7+xuzy+Jt4rB15ciA8Tmbivsov9YW+Y4lGbweRBmryfM0qjlIX7JO9cW4jxjbJ7LWt45+/YMffNoFaSOBP2VbzoZkKlsLvkvMtWjr1hR31y8+55HOf3a1RPJxBS3qUyfCR9CXUB9vvuzr6/infx/cl2V7uoy5u5aZCgfsNSaXDi92PX8NKHkv+5FEhUW8GreB941+0uIVub4RX9M1F1AR1iyo+jymvTdW03/n67Gu/a++074T7/TDFtxEvguaWVe0oZ5hneyGZYjevkZ2fheOmw2jPtjoHXgkeyvVHw/X3K3Dr3/sqEp6enrenkBQWLWDNF2rj7jG/r+j3OC4ekSZ9zJcvrMy0Tjb8/6nGcn4bzo596/Qf4ByXlWSrFGMlaAAAAAElFTkSuQmCC'
+# # button list types
+# if show_edit_button is True:
+#     installed_button_list = ['uninstall', 'credits', 'scripts', 'edit']
+#     uninstalled_button_list = ['uninstall', 'credits', 'scripts', 'edit']
+#     corrupted_button_list = ['install', 'uninstall', 'errors', 'credits', 'scripts', 'edit']
+# else:
+#     installed_button_list = ['uninstall', 'credits', 'scripts']
+#     uninstalled_button_list = ['uninstall', 'credits', 'scripts']
+#     corrupted_button_list = ['install', 'uninstall', 'errors', 'credits', 'scripts']
 
 
 # Create column layouts
@@ -269,8 +351,10 @@ search_column = [
     ],
     [
         sg.Text("Search apps: ", font=default_font),
-        sg.In(size=(25, 1), enable_events=True, font=default_font, key='-SEARCH BAR-'),
-        sg.Button("Search", font=default_font, key='-SEARCH-', bind_return_key=True),
+        sg.In(size=(25, 1), enable_events=True,
+              font=default_font, key='-SEARCH BAR-'),
+        sg.Button("Search", font=default_font,
+                  key='-SEARCH-', bind_return_key=True),
     ],
     [
         sg.Text('')
@@ -308,17 +392,19 @@ Introducing Pi-Apps, a well-maintained collection of app installation-scripts th
 
 Pi-Apps now serves over 1,000,000 people and hosts nearly 200 apps.""", key="-DESCRIPTION-", size=(75, 20), font=default_font)],
 
-    [sg.Column([[sg.Button(key="-CREDITS-",
-               button_text="Credits", font=default_font, tooltip='See who made the app and who put it on Pi-Apps', visible=False)]], pad=(0, 0)),
-     sg.Column([[sg.Button(key="-SCRIPTS-",
-               tooltip="Feel free to see how an app is installed or uninstalled!\nPerfect for learning or troubleshooting.", button_text='   ', image_filename=f'{DIRECTORY}/icons/shellscript.png', font=default_font, visible=False,)]]),
-     sg.Column([[sg.Button(key="-ERRORS-", tooltip="Errors", button_text='   ',
-               image_filename=f'{DIRECTORY}/icons/log-file.png', font=default_font, visible=False,)]]),
-     sg.Text('',pad=(50, 0)),
-     sg.Column([[sg.Button(key="-UNINSTALL-",
-               image_filename=f'{DIRECTORY}/icons/uninstall.png', button_text="  ", visible=False)]]),
-     sg.Column([[sg.Button(key="-INSTALL OR UNINSTALL-",
-               image_filename=f'{DIRECTORY}/icons/install.png', button_text="  ", visible=False)]]),
+    [sg.Column([[sg.Button(key="-BUTTON 1-", font=default_font,
+               image_filename=f'{DIRECTORY}/icons/shellscript.png', button_text="Credits", visible=False)]], pad=(0, 0)),
+     sg.Column([[sg.Button(key="-BUTTON 2-", font=default_font,
+               button_text='   ', image_filename=f'{DIRECTORY}/icons/shellscript.png', visible=False,)]]),
+     sg.Column([[sg.Button(key="-BUTTON 3-", font=default_font,  tooltip="Errors", button_text='   ',
+               image_filename=f'{DIRECTORY}/icons/shellscript.png', visible=False,)]]),
+     sg.Column([[sg.Button(key="-BUTTON 4-", font=default_font,
+               image_filename=f'{DIRECTORY}/icons/shellscript.png', visible=False)]], pad=(0, 0)),
+     sg.Column([[sg.Button(key="-BUTTON 5-", font=default_font,
+               image_filename=f'{DIRECTORY}/icons/shellscript.png', visible=False)]], pad=(0, 0)),
+     sg.Column([[sg.Button(key="-BUTTON 6-", font=default_font,
+               image_filename=f'{DIRECTORY}/icons/shellscript.png', visible=False)]], pad=(0, 0)),
+
      ]
 ]
 
@@ -414,7 +500,7 @@ while True:
                 ids = window['-APP LIST-'].Widget.identify_row(1)
                 key = window['-APP LIST-'].IdToKey[ids]
                 window['-APP LIST-'].Widget.selection_set(ids)
-                
+
                 window['-APP LIST-'].set_focus()
 
             else:  # search query is empty, back to category
@@ -428,22 +514,22 @@ while True:
             back_to_category_list()
 
     elif event == "-APP LIST-":  # An app was chosen from the list
-
         # Get current selected item
         if window.Element('-APP LIST-').SelectedRows != []:
             current_app = app_list[window.Element(
                 '-APP LIST-').SelectedRows[0]]
-
+        print(current_app)
         if current_app in app_categories:  # If the item is a category, enter the category
             current_category = current_app
             show_category(current_category)
-        else:
-            try:
-                app = current_app
-                load_app_info(app)
+        elif current_app in os.popen(
+            f'{DIRECTORY}/api list_apps cpu_installable | {DIRECTORY}/api list_intersect "$({DIRECTORY}/api list_apps visible)" | {DIRECTORY}/api list_intersect "$({DIRECTORY}/api list_apps cpu_installable)"').read().split('\n'):
+            # try:
+            app = current_app
+            load_app_info(app)
 
-            except:
-                pass
+            # except:
+            #     pass
 
     elif event == '-DESCRIPTION-+FOCUS_IN+':  # When focus in description box, remove caret
         window['-DESCRIPTION-'].Widget.bind("<1>",
@@ -503,67 +589,66 @@ while True:
     elif event == '-APP LIST-+MOUSE AWAY+':  # When mouse leave app list, clear tooltips
         window['-TOOLTIP-'].update('')
 
-    elif event == '-INSTALL-':
-        if current_app != '' and current_app not in app_categories:
-            app = current_app
-            install_app(app)
-            load_app_info(app)
-        else:
-            pass
-
-    elif event == '-UNINSTALL-':
-        if current_app != '' and current_app not in app_categories:
-            app = current_app
-            uninstall_app(app)
-            load_app_info(app)
-        else:
-            pass
-
-    elif event == '-CREDITS-':  # Popup window
-        if current_app != '':
-            if current_app not in app_categories:
+    elif event == '-BUTTON 1-' or  event == '-BUTTON 2-' or  event == '-BUTTON 3-' or event == '-BUTTON 4-' or event == '-BUTTON 5-' or event == '-BUTTON 6-' :
+        operation = button_list[int(''.join([n for n in event if n.isdigit()])) - 1]
+        
+        if operation == 'install':
+            if current_app != '' and current_app not in app_categories:
                 app = current_app
-                credits_text = os.popen(
-                    f'cat "{DIRECTORY}/apps/' + app + '/credits" 2>/dev/null').read().rstrip('\n')
-                if credits_text != '':
-                    sg.Window('Credits', [[sg.T(credits_text)], [
-                              sg.OK(s=10)]]).read(close=True)
-                else:
-                    sg.Window('Credits', [
-                              [sg.T('No credit found for ' + app + '.\n')], [sg.OK(s=10)]]).read(close=True)
+                install_app(app)
+                load_app_info(app)
             else:
-                pass  # category is selected, no credits available, skip
-        else:
-            pass
-
-    elif event == '-SCRIPTS-':
-        if current_app != '':
-            if current_app not in app_categories:
+                pass
+        elif operation == 'uninstall':
+            if current_app != '' and current_app not in app_categories:
                 app = current_app
-                if os.popen(f'{DIRECTORY}/api app_type ' + app).read().strip('\n') != 'package':
-                    for file in ['install', 'install-32', 'install-64', 'uninstall']:
-                        if os.path.exists(f'{DIRECTORY}/apps/' + app + '/' + file):
-                            os.popen(
-                                f'{DIRECTORY}/api text_editor "{DIRECTORY}/apps/' + app + '/' + file + '"')
-                else:
-                    sg.Window('No scripts found', [[sg.T(
-                        app + ' is a package app.\n')], [sg.OK(s=10)]], size=(500, 100)).read(close=True)
-
+                uninstall_app(app)
+                load_app_info(app)
             else:
-                pass  # category is selected
-        else:
-            pass
+                pass
+        elif operation == 'credits':
+            if current_app != '':
+                if current_app not in app_categories:
+                    app = current_app
+                    credits_text = os.popen(
+                        f'cat "{DIRECTORY}/apps/' + app + '/credits" 2>/dev/null').read().rstrip('\n')
+                    if credits_text != '':
+                        sg.Window('Credits', [[sg.T(credits_text)], [
+                                sg.OK(s=10)]]).read(close=True)
+                    else:
+                        sg.Window('Credits', [
+                                [sg.T('No credit found for ' + app + '.\n')], [sg.OK(s=10)]]).read(close=True)
+                else:
+                    pass  # category is selected, no credits available, skip
+            else:
+                pass
+        elif operation == 'scripts':
+            if current_app != '':
+                if current_app not in app_categories:
+                    app = current_app
+                    if os.popen(f'{DIRECTORY}/api app_type ' + app).read().strip('\n') != 'package':
+                        for file in ['install', 'install-32', 'install-64', 'uninstall']:
+                            if os.path.exists(f'{DIRECTORY}/apps/' + app + '/' + file):
+                                os.popen(
+                                    f'{DIRECTORY}/api text_editor "{DIRECTORY}/apps/' + app + '/' + file + '"')
+                    else:
+                        sg.Window('No scripts found', [[sg.T(
+                            app + ' is a package app.\n')], [sg.OK(s=10)]], size=(500, 100)).read(close=True)
+
+                else:
+                    pass  # category is selected
+            else:
+                pass
+        elif operation == 'errors':
+            if current_app != '':
+                app = current_app
+                print(app)
+                error_file = os.popen(
+                    f'ls "{DIRECTORY}/logs"/* -t | grep "fail-' + app + '" | head -n1').read()
+                os.popen(f'{DIRECTORY}/api text_editor ' + error_file)
 
     elif event == '-GO TO SEARCH-':  # When alt+s is pressed, focus to search bar
         window['-SEARCH BAR-'].set_focus()
-
-    elif event == '-ERRORS-':  # Show error report
-        if current_app != '':
-            app = current_app
-            print(app)
-            error_file = os.popen(
-                f'ls "{DIRECTORY}/logs"/* -t | grep "fail-' + app + '" | head -n1').read()
-            os.popen(f'{DIRECTORY}/api text_editor ' + error_file)
 
     elif event == '-GITHUB BUTTON-':
         webbrowser.open('https://github.com/Botspot/pi-apps')
